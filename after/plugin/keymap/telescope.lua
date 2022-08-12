cc68240644bed0e8f01d1bb6da3b5e6b9e337fd6
Andrea Talignani <talignania@QS-NB-AndTal.qsinformatica.local>
2022-08-12 14:17:15 +0200

    telescope find current word keymap

--- after/plugin/keymap/telescope.lua
+++ after/plugin/keymap/telescope.lua
@@ -2,6 +2,8 @@ local nnoremap = require('andrea.keymap').nnoremap
 
 nnoremap('<leader>ff', function() require('telescope.builtin').find_files() end, { desc = 'Search files' })
 nnoremap('<leader>fw', function() require('telescope.builtin').live_grep() end, { desc = 'Search a word' })
+nnoremap('<leader>fcw', function() require('telescope.builtin').grep_string({ search = vim.fn.expand("<cword>") }) end,
+    { desc = 'Search for current word' })
 nnoremap('<leader>fb', function() require('telescope.builtin').buffers() end, { desc = 'Search a buffer' })
 nnoremap('<leader>sd', function() require('telescope.builtin').diagnostics({ bufnr = 0 }) end,
     { desc = 'Show diagnostics' })
